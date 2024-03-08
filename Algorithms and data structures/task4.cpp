2756a446352fc288dcec85cd3b5118f5e58699ee
stt1xx <stt1xx@mail.ru>
2024-03-08 19:39:13 +0300

    add report1-4

--- Algorithms and data structures/task4.cpp
+++ Algorithms and data structures/task4.cpp
@@ -5,30 +5,30 @@ using namespace std;
 
 int main(){
     long days;
-    double result, capacity, start, multiplier, destroy;
+    double result, capacity, start, multiplier, destruction;
 
-    cin >> start >> multiplier >> destroy >> capacity >> days;
+    cin >> start >> multiplier >> destruction >> capacity >> days;
 
     if (multiplier == 1){
-        result = start - destroy * days;
+        result = start - destruction * days;
         cout << max(0.0, result);
         return 0;
     }
 
-    if (multiplier - destroy / start > 1 && (days >= capacity - start || start >= capacity)){
+    if (multiplier - destruction / start > 1 && (days >= capacity - start || start >= capacity)){
         cout << capacity;
         return 0;
     }
-    if (multiplier - destroy / start < 1 && (days > capacity || days >= start)){
+    if (multiplier - destruction / start < 1 && days >= start){
         cout << 0;
         return 0;
     }
-    if (multiplier - destroy / start == 1){
-        cout << start;
+    if (multiplier - destruction / start == 1){
+        cout <<  min(start, capacity);
         return 0;
     }
 
-    result = start * pow(multiplier, days) - destroy * (pow(multiplier, days)  - 1) / (multiplier - 1);
+    result = start * pow(multiplier, days) - destruction * (pow(multiplier, days)  - 1) / (multiplier - 1);
     cout << min(max(result, 0.0), capacity);
     return 0;
 }
