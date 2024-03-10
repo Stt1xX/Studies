4c6c506059075d7232b658eda86a39fc338bfa6b
stt1xx <stt1xx@mail.ru>
2024-03-10 22:43:43 +0300

    minor changes

--- Computational mathematics/main/main.py
+++ Computational mathematics/main/main.py
@@ -1,12 +1,13 @@
 from entity import Equations_system
 from scanner import Scanner
 from solver import Solver
+from utils import bcolors
 
 first_equations_system = Equations_system()
 solver = Solver()
 scanner = Scanner()
 
-print("   _____ _____ ______        _     _____       _   _             \n  / ____|  __ \\___  /       | |   |  __ \\     | | (_)            \n | |  __| |  | | / /    ___ | |_  | |__) |   _| |_ _ _ __   __ _ \n | | |_ | |  | |/ /    / _ \\| __| |  ___/ | | | __| | '_ \\ / _` |\n | |__| | |__| / /__  | (_) | |_  | |   | |_| | |_| | | | | (_| |\n  \\_____|_____/_____|  \\___/ \\__| |_|    \\__,_|\\__|_|_| |_|\\__,_|\n                                                                 \n                                                                 ")
+print(f"{bcolors.OKGREEN}   _____ _____ ______        _     _____       _   _             \n  / ____|  __ \\___  /       | |   |  __ \\     | | (_)            \n | |  __| |  | | / /    ___ | |_  | |__) |   _| |_ _ _ __   __ _ \n | | |_ | |  | |/ /    / _ \\| __| |  ___/ | | | __| | '_ \\ / _` |\n | |__| | |__| / /__  | (_) | |_  | |   | |_| | |_| | | | | (_| |\n  \\_____|_____/_____|  \\___/ \\__| |_|    \\__,_|\\__|_|_| |_|\\__,_|\n                                                                 \n                                                                 {bcolors.ENDC}")
 
 scanner.initialize_task(first_equations_system)
 
