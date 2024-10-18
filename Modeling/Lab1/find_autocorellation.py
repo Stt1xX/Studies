20a77b978560fa04bb432b145267b535bb77e94b
Timur <stt1xx@mail.ru>
2024-10-18 05:48:49 +0300

    delete FUCKING LINUX

---
+++ Modeling/Lab1/find_autocorellation.py
@@ -0,0 +1,54 @@
+import openpyxl
+import numpy as np
+
+PATH = "MainExcelFile.xlsx"
+N = 300
+NUMBER_OF_SHIFTS = 10 # значение должно быть меньше чем N хотя бы на 3 
+
+def get_values_from_xlsx():
+    specified_set = []
+    generated_set = []
+    wb_obj = openpyxl.load_workbook(PATH)
+    for i in range(1, N + 1):
+        specified_set.append(wb_obj.active.cell(row = i, column = 2).value)
+        generated_set.append(wb_obj.active.cell(row = i, column = 3).value)
+    return specified_set, generated_set
+
+
+def find_corell_coefs(values):
+    ret_arr = []
+    for i in range(1, min(NUMBER_OF_SHIFTS + 1, N - 2)): 
+        selection1 = values[:-i]
+        selection2 = values[i:]
+        mean1 = np.mean(selection1)
+        mean2 = np.mean(selection2)
+        top = 0
+        bottom_left = 0
+        bottom_right = 0
+        for j in range(len(selection1)):
+            curr_diff1 = selection1[j] - mean1
+            curr_deff2 = selection2[j] - mean2
+            top += curr_diff1 * curr_deff2
+            bottom_left += curr_diff1**2
+            bottom_right += curr_deff2**2
+        coef = top / np.sqrt(bottom_left * bottom_right)
+        ret_arr.append(coef.item())
+    return ret_arr
+
+def print_values(values, header):
+    print("----- " + header + " ------")
+    for i in range(len(values)):
+        print(i + 1, ":", round(values[i], 4))
+    print("Max:", max(values))
+    print("Min:", min(values))
+    print("----------------------------\n")
+
+def solve(specified_set, generated_set):
+    print_values(find_corell_coefs(generated_set), "Заданная числовая последовательность")
+    print_values(find_corell_coefs(specified_set), "Сгенерированная числовая последовательность")
+
+
+tmp_set = get_values_from_xlsx()
+# Сгенерированная ЧП
+solve(tmp_set[0], tmp_set[1])
+
---
+++ Modeling/Lab1/generator.py
@@ -0,0 +1,19 @@
+from scipy.stats import expon
+import random
+
+A1_value = 42.348805	
+A2_value = 1.341817457
+q_border = 0.6 # вероятность формирования сл. величины  в первой фазе
+VOLUME = 300
+
+
+def generate_values():
+    result_arr = []
+    for i in range(VOLUME + 1):
+        if random.random() >= q_border:
+            result_arr.append(expon.rvs(scale=A2_value, loc=0))
+        else:
+            result_arr.append(expon.rvs(scale=A1_value, loc=0))
+    return result_arr
+
+print(generate_values())
